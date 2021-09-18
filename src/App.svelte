152b3ecfe889239b64c8ab9ecbd0a5dfe21a572b
nottnt <tanatat19@outlook.com>
2021-09-19 01:04:04 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+	import adapter from 'webrtc-adapter';
+	import VideoRecord from './VideoRecord.svelte'
+
+	const userAgent = navigator.userAgent;
+	const detectOpenWebOnLineApp = () => {
+		const userAgentSplitToArr = userAgent.split(' ');
+		const userAgentSplitToArrLast = userAgentSplitToArr[userAgentSplitToArr.length - 1]
+
+		return userAgentSplitToArrLast.includes('Line')
+	}
+
+	if (detectOpenWebOnLineApp()) {
+		window.open(window.location , '_system');
+	}
+
+	export const getCurrentWebOpen = () => {
+		return detectOpenWebOnLineApp() ? 'Line Application' : 'Itself';
+	}
+
+	export let name;
+	$: adapter
+</script>
+<link rel="stylesheet" href="main.css" />
+
+<header>
+	WebRTC Experiment🧪
+</header>
+<main>
+	<div class="flex-container">
+		<div class="row"> 
+			<VideoRecord />
+			<div class="div-table">
+				<div class="div-table-row">
+					<div class="div-table-col header" align="center">Browser</div>
+					<div class="div-table-col header">Version</div>
+					<div class="div-table-col header">Run On</div>
+				</div>
+				<div class="div-table-row">
+					<div class="div-table-col">{adapter.browserDetails.browser}</div>
+					<div class="div-table-col">{adapter.browserDetails.version}</div>
+					<div class="div-table-col">{getCurrentWebOpen()}</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+<footer>
+	Copyright © 2021 Nottnt. 
+	<img src="/sad-cat.png" alt="Girl in a jacket" width="25" height="25">
+</footer>
---
+++ src/VideoRecord.svelte
@@ -0,0 +1,104 @@
+<script>
+    import { onMount } from 'svelte';
+    import RecordRTC from './RecordRTC.js';
+    let videoRecorder;
+    let videoRecorded;
+    let videoRecordedURL;
+    let recorder;
+
+    onMount(() => {
+        videoRecorder  = document.getElementById("video-recorder");
+        videoRecorded = document.getElementById("video-recorded");
+	});
+
+    export const handleRecordVideo = () => {
+        const recordingTimeMS = 6000;
+        console.log(videoRecorder)
+        videoRecorder.style.display = "block"; // display video for record
+        videoRecorded.style.display = "none"; // hide video recorded
+        videoRecorder.style.backgroundColor = "black"
+        videoRecorded.style.backgroundColor = "black";
+
+        if (videoRecordedURL) { // check for revoke obj url
+            URL.revokeObjectURL(videoRecordedURL)
+        }
+
+        captureCamera(function(camera) {
+            videoRecorder.muted = true;
+            videoRecorder.volume = 0;
+            videoRecorder.srcObject = camera;
+
+            recorder = RecordRTC(camera, {
+                type: 'video',
+                audio: false,
+                video: true
+            });
+
+            recorder.startRecording();
+
+            // release camera on stopRecording
+            recorder.camera = camera;
+
+            setTimeout(function () {
+                recorder.stopRecording(stopRecordingCallback);
+            }, recordingTimeMS)
+        })
+    }
+
+    const captureCamera = (callback) => {
+        const constraints = { video: true };
+
+        navigator.mediaDevices.getUserMedia(constraints).then(function(camera) {
+        callback(camera);
+        }).catch(function(error) {
+        console.error(error);
+
+        if (error.name.indexOf('NotAllowedError') !== -1) {
+            onErrorGetAccessCamera()
+        }
+        });
+    }
+
+    const stopRecordingCallback = () => {
+        const recordedBlob = recorder.getBlob()
+        console.log('recordedBlob', recordedBlob)
+        videoRecordedURL = URL.createObjectURL(recordedBlob);
+        videoRecorder.muted = true;
+        videoRecorded.src = videoRecordedURL
+        recorder.camera.stop();
+        recorder.destroy();
+        recorder = null;
+
+        videoRecorder.style.display = "none"; // hide video recorder
+        videoRecorded.style.display = "block"; // display video recorded
+        // Assign video file to `input file for prepare to upload`
+    }
+
+</script>
+<link rel="stylesheet" href="video-verify-identity.css" />
+
+<div id="video-recorder-container" class="video-recorder-container">
+    <content class="video-recorder-content-container">
+        <div class="content-wrapper">
+            <div class="video-recorder-header-message">
+                <span>Record Video</span>
+            </div>
+            <div class="video-recorder-wrapper">
+                <video id="video-recorder" class="video-recorder" autoplay muted playsinline></video>
+                <video id="video-recorded" class="video-recorder" controls style="display: none"></video>
+            </div>
+            <div class="start-verify-button-wrapper">
+                <button class="start-verify-button" on:click={handleRecordVideo}>
+                    <span>Start Recording</span>
+                </button>
+            </div>
+            <div class="message-wrapper">
+                <div class="hint-message">
+                    <span>Note: Recording for 5 seconds when you click start recording </span>
+                </div>
+            </div>
+            <p id="not-verify-message" class="upload-document-error" style="display: none; margin-bottom: 10px;">กรุณายืนยันตัวตน</p>
+            <p id="video-verify-success-message" class="upload-success" style="display: none; margin-bottom: 10px;">วิดีโอของท่านได้รับการบันทึกเรียบร้อยแล้ว</p>
+        </div>
+    </content>
+</div>
